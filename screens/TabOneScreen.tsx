730bd7c3d0dd4075fe05405b18d9e2a2e2258bf7
Adrien Masson <adrien.masson@hotmail.fr>
2022-08-10 21:12:53 +0200

    type navigation files

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet } from "react-native";
 import { Text, View } from "../components/Themed";
-import { RootTabScreenProps } from "../types";
 
 export default function TabOneScreen() {
 	return (
